e0a5f2ad5e07b5aeccb7c31fa61c8cd8439e5aa0
ncqhuy <nolannguyen020705@gmail.com>
2026-04-22 15:05:16 +0700

    Fix: sua loi flake8 lint

--- BT1_QEMU_SystemMonitor/system_monitor.py
+++ BT1_QEMU_SystemMonitor/system_monitor.py
@@ -1,9 +1,7 @@
 import psutil
-from datetime import datetime
-from time import sleep
 
 # Bước 2: Đọc CPU usage
 cpu_list = psutil.cpu_percent(interval=1, percpu=True)
-cpu_avg = sum(cpu_list) / len(cpu_list) # Tính trung bình các nhân
+cpu_avg = sum(cpu_list) / len(cpu_list)  # Tính trung bình các nhân
 print(f"CPU List: {cpu_list}")
 print(f"CPU Average: {cpu_avg}%")
---
+++ BT3_Wokwi_GPIO/README.md
@@ -0,0 +1,8 @@
+bt3_step2:
+https://wokwi.com/projects/461967045823074305
+
+bt3_step4:
+https://wokwi.com/projects/461967327776866305
+
+bt3_step5:
+https://wokwi.com/projects/461966839054320641
